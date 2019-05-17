0a00c258526a8f2b9aea173bdc5a675db5f201a4
Kevin J. Dugan <dugankj@ornl.gov>
2019-05-16 21:39:04 -0400

    Refactoring mpi prefix formatting

--- Nucleus/Nucleus.cpp
+++ Nucleus/Nucleus.cpp
@@ -3,7 +3,26 @@
 
 Nucleus::Nucleus(){}
 
-void Nucleus::OnTestStart(const ::testing::TestInfo& info) {
+std::tuple<std::string, unsigned int, unsigned int> Nucleus::getMPIprefix(const color& c, const std::string& step, const align& alignment) const {
+
+  // Figure out coloring
+  std::string colorStart, colorEnd = "";
+  switch (c){
+    case color::GREEN:
+      colorStart = "\e[32m";
+      colorEnd = "\e[0m";
+      break;
+    case color::RED:
+      colorStart = "\e[31m";
+      colorEnd = "\e[0m";
+      break;
+    case color::YELLOW:
+      colorStart = "\e[33m";
+      colorEnd = "\e[0m";
+      break;
+    default:
+      break;
+  }
 
   // Set up Rank info string
   int rank, size;
@@ -12,17 +31,58 @@ void Nucleus::OnTestStart(const ::testing::TestInfo& info) {
   int nDigits = floor(log10(size) + 1);
   std::string procString = std::to_string(size);
   procString.insert(procString.begin(), nDigits-procString.length(), ' ');
-  procString = "[" + procString + " Procs]";
+  procString = "[ " + procString + " ";
+  if (size == 1)
+    procString += "Proc";
+  else
+    procString += "Procs";
+  procString += " ]";
 
-  // Setup start info
-  std::string message = "\e[32m";
-  message += procString + "[ RUN      ] \e[0m";
+  // Format step info
+  std::string stepString;
+  if (step.length() == 1){
+    stepString = std::string(step.c_str(), 10);
+    stepString = "[" + stepString + "] ";
+  } else {
+    // stepString = std::toupper(step);
+    stepString = step;
+    std::transform(stepString.begin(), stepString.end(), stepString.begin(), ::toupper);
+    unsigned int length = stepString.length();
+    switch (alignment){
+      case align::LEFT:
+        stepString.append(8-length, ' ');
+        break;
+      case align::RIGHT:
+        stepString.insert(stepString.begin(), 8-length, ' ');
+        break;
+      case align::CENTER:
+        stepString.insert(stepString.begin(), (8-length)/2, ' ');
+        stepString.append((8-length)/2, ' ');
+        break;
+      default:
+        break;
+    }
+    stepString = "[ " + stepString + " ] ";
+  }
+
+  std::string message;
+  message = colorStart + procString + stepString + colorEnd;
+
+  return std::make_tuple(message, rank, size);
+
+}
+
+void Nucleus::OnTestStart(const ::testing::TestInfo& info) {
+
+  auto prefix = this->getMPIprefix(color::GREEN, "run", align::LEFT);
+
+  std::string message = std::get<0>(prefix);
   message += info.test_case_name();
   message += ".";
   message += info.name();
   message += "\n";
 
-  this->OutputMessage(message, rank, size);
+  this->OutputMessage(message, std::get<1>(prefix), std::get<2>(prefix));
 }
 
 void Nucleus::OutputMessage(std::string message,
@@ -49,19 +109,15 @@ std::string Nucleus::PrintFullTestCommentIfPresent(const ::testing::TestInfo& te
   std::string result;
   if (type_param != NULL || value_param != NULL) {
     result += ", where ";
-    // printf(", where ");
     if (type_param != NULL) {
       result += "TypeParam = ";
       result += type_param;
-      // printf("%s = %s", "TypeParam", type_param);
       if (value_param != NULL)
         result += " and ";
-        // printf(" and ");
     }
     if (value_param != NULL) {
       result += "GetParam() = ";
       result += value_param;
-      // printf("%s = %s", "GetParam()", value_param);
     }
   }
 
@@ -69,21 +125,14 @@ std::string Nucleus::PrintFullTestCommentIfPresent(const ::testing::TestInfo& te
 }
 
 void Nucleus::OnTestEnd(const ::testing::TestInfo& info) {
-  // Set up Rank info string
-  int rank, size;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-  int nDigits = floor(log10(size) + 1);
-  std::string procString = std::to_string(size);
-  procString.insert(procString.begin(), nDigits-procString.length(), ' ');
-  procString = "[" + procString + " Procs]";
 
-  std::string message;
-  if (info.result()->Passed()){
-    message = "\e[32m" + procString + "[       OK ] \e[0m";
-  } else {
-    message = "\e[31m" + procString + "[  FAILED  ] \e[0m";
-  }
+  std::tuple<std::string, unsigned int, unsigned int> prefix;
+  if (info.result()->Passed())
+    prefix = this->getMPIprefix(color::GREEN, "ok", align::RIGHT);
+  else
+    prefix = this->getMPIprefix(color::RED, "failed", align::CENTER);
+
+  std::string message = std::get<0>(prefix);
   message += info.test_case_name();
   message += ".";
   message += info.name();
@@ -93,7 +142,7 @@ void Nucleus::OnTestEnd(const ::testing::TestInfo& info) {
 
   message += " (" + std::to_string(info.result()->elapsed_time()) + " ms)\n";
 
-  this->OutputMessage(message, rank, size);
+  this->OutputMessage(message, std::get<1>(prefix), std::get<2>(prefix));
 }
 
 void Nucleus::OnTestPartResult(const ::testing::TestPartResult& result) {
@@ -107,7 +156,7 @@ void Nucleus::OnTestPartResult(const ::testing::TestPartResult& result) {
   int nDigits = floor(log10(size) + 1);
   std::string rankString = std::to_string(rank);
   rankString.insert(rankString.begin(), nDigits-rankString.length(), ' ');
-  rankString = "[Rank " + rankString + " ]";
+  rankString = "[ Rank " + rankString + " ]";
 
   std::string message;
   message += "\e[31m" + rankString + "\e[0m\n";
@@ -131,6 +180,29 @@ void Nucleus::OnTestPartResult(const ::testing::TestPartResult& result) {
 
 }
 
+void Nucleus::OnTestCaseStart(const ::testing::TestCase& test_case) {
+  // std::cout << "Case Start" << std::endl;
+}
+
+void Nucleus::OnTestCaseEnd(const ::testing::TestCase& test_case) {
+  // std::cout << "Case End" << std::endl;
+}
+
+void Nucleus::OnTestIterationStart(const ::testing::UnitTest& unit_test, int iteration){
+  
+}
+
+void Nucleus::OnTestIterationEnd(const ::testing::UnitTest& unit_test, int iteration){
+  // std::cout << "Iteration End" << std::endl;
+}
+
+void Nucleus::OnEnvironmentsSetUpStart(const ::testing::UnitTest& unit_test){
+  // std::cout << "Environment setup" << std::endl;
+}
+
+void Nucleus::OnEnvironmentsTearDownStart(const ::testing::UnitTest& unit_test){
+  // std::cout << "Environment teardown" << std::endl;
+}
 
 
 int main(int argc, char **argv) {
--- Nucleus/Nucleus.h
+++ Nucleus/Nucleus.h
@@ -10,6 +10,12 @@ class Nucleus : public ::testing::EmptyTestEventListener {
     public:
         Nucleus();
 
+        virtual void OnTestIterationStart(const ::testing::UnitTest& unit_test, int iteration) override;
+        virtual void OnTestIterationEnd(const ::testing::UnitTest& unit_test, int iteration) override;
+        virtual void OnEnvironmentsSetUpStart(const ::testing::UnitTest& unit_test) override;
+        virtual void OnEnvironmentsTearDownStart(const ::testing::UnitTest& unit_test) override;
+        virtual void OnTestCaseStart(const ::testing::TestCase& test_case) override;
+        virtual void OnTestCaseEnd(const ::testing::TestCase& test_case) override;
         virtual void OnTestStart(const ::testing::TestInfo& info) override;
         virtual void OnTestEnd(const ::testing::TestInfo& info) override;
         virtual void OnTestPartResult(const ::testing::TestPartResult& result) override;
@@ -20,6 +26,11 @@ class Nucleus : public ::testing::EmptyTestEventListener {
                            const unsigned int rank,
                            const unsigned int size,
                            const bool outputAllProcs = false) const;
+
+        enum color {GREEN, RED, YELLOW};
+        enum align {LEFT, RIGHT, CENTER};
+        std::tuple<std::string, unsigned int, unsigned int> getMPIprefix(const color &c, const std::string &step, const align &alignment) const;
+ 
 };
 
 #endif // NUCLEUS_H_12LO
