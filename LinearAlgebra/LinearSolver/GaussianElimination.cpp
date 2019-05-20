19faebdd3c511a2840e2bedc2dd146e4e70046cb
Kevin Dugan <dugankj@ornl.gov>
2019-05-20 14:47:01 -0400

    Update GaussianElimination.cpp

--- LinearAlgebra/LinearSolver/GaussianElimination.cpp
+++ LinearAlgebra/LinearSolver/GaussianElimination.cpp
@@ -11,30 +11,56 @@ std::unique_ptr<AbstractVector> GaussianElimination::solve(const AbstractMatrix 
     std::unique_ptr<AbstractMatrix> localA = A.clone();
     std::unique_ptr<AbstractVector> localb = b.clone();
 
-    for (unsigned int col = 0; col < 1; col++){
-        // header = Bcast( row corresponding to col)
+    IndexRange range = localA->getGlobalRowIndexRange();
+    std::vector<unsigned int> part = localA->getPartitionSize();
+
+    // Upper Triangular Transformation
+    std::vector<double> rhs_values(localb->size());
+    localb->getGlobalValues(rhs_values);
+
+    for (unsigned int col = 0; col < 6/*localA->nCols()*/; col++){
         std::vector<double> header = localA->getRowValues(col);
+        double rhs_header = localb->getValue(col);
 
-        // for each local row (i)
-        //      F = -1.0 * localA->getValue(i, col) / header[col]
-        //      for each entry (j)
-        //          localA->setLocalValue(i, j) = localA->getLocalValue(i,j) + F * header[j]
+        for (unsigned int local_row = 0, global_row = range.begin; local_row < part[this->linalg->rank()]; local_row++, global_row+=range.skip){
+            if (global_row <= col)
+              continue;
 
-        unsigned int start_local = 0;
-
-
-        std::vector<unsigned int> part = localA->getPartitionSize();
-        for (unsigned int local_row = start_local; local_row < part[this->linalg->rank()]; local_row++){
-            std::vector<double> localRow = localA->getLocalRowValues(local_row);
-            double factor = -1.0 * localRow[col] / header[col];
-            for (unsigned int entry = col; entry < localRow.size(); entry++){
-                localRow[entry] += factor * header[entry];
+            std::vector<double> local_row_values = localA->getLocalRowValues(local_row);
+            double factor = -1.0 * local_row_values[col] / header[col];
+            for (unsigned int entry = col; entry < local_row_values.size(); entry++){
+                local_row_values[entry] += factor * header[entry];
             }
-            localA->setLocalRowValues(local_row, localRow);
+            localA->setLocalRowValues(local_row, local_row_values);
+
+            rhs_values[global_row] += factor * rhs_header;
         }
+        localb->setValues(rhs_values);
     }
 
-    localA->print();
+    // Back Substitution
+    std::vector<double> result_values(result->size());
+    localb->getGlobalValues(result_values);
+    for (int row = localb->size()-1; row >= 0; row--){
+        unsigned int rowOnRank = result->findRankWithIndex(row);
+
+        double factor = 0.0;
+        if (this->linalg->rank() == rowOnRank){
+            unsigned int local_row = (row - range.begin)/range.skip;
+            double denom = localA->getLocalRowValues(local_row)[row];
+            factor = result_values[row] / denom;
+            result_values[row] = factor;
+        }
+        MPI_Bcast(&factor, 1, MPI_DOUBLE, rowOnRank, MPI_COMM_WORLD);
+        for (unsigned int local_row = 0, global_row = range.begin; local_row < part[this->linalg->rank()]; local_row++, global_row+=range.skip){
+            if (global_row >= row)
+                continue;
+
+            std::vector<double> local_row_values = localA->getLocalRowValues(local_row);
+            result_values[global_row] -= local_row_values[row] * factor;
+        }
+    }
+    result->setValues(result_values);
 
     return result;
 }
